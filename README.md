965138df1a3c632304ee4c243a3f9b57b022665d
Ismael Ramos <ramossilvanismael@gmail.com>
2018-03-17 22:13:04 +0100

    feat(gulp): added gulp tasks to livereload, and browser sync

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 # Example project to build a static website with AMP + Nunjucks + Gulp
 
-> ### Base project made with much  :heart: . Contains amp components, and a free template. Yes, AMP valid.
+> ### Base project made with much  :heart:. Contains amp components, and a free template. Yes, AMP validated.
 
 ## Usage
 
 `npm i` - Installs everything needed
 
-`npm run build` - Creates dist folder ready to deploy everywhere
+`npm run start` - Runs the server and watch for file changes in src folder. localhost:8000
+
+`npm run build` - Creates dist folder ready to deploy it
+
+`npm run validate` - Validate all html files inside dist folder to be AMP valid
 
 ## Features
-* Nunjucks templates with structured data
-* Custom fonts
+* Nunjucks templates
+* Gulp tasks for development server, watch changes, livereload, minify files, clean dist folder, etc
 * Free template
 * CSS separated and mixed and minified with gulp tasks
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,69 @@
 'use strict';
 
 var gulp = require('gulp'),
+  runSequence = require('run-sequence'),
   nunjucksRender = require('gulp-nunjucks-render'),
   htmlmin = require('gulp-htmlmin'),
   clean = require('gulp-clean'),
   pump = require('pump'),
-  sourceFolder = 'src',
-  distFolder = 'dist';
+  connect = require('gulp-connect'),
+  watch = require('gulp-watch'),
+  gulpAmpValidator = require('gulp-amphtml-validator'),
+  open = require('gulp-open');
 
-gulp.task('default', function () {
-  return gulp.src(distFolder, {read: false, force: true})
+var config = {
+  port: 8000,
+  sourceFolder: 'src',
+  distFolder: 'dist'
+};
+
+gulp.task('development', ['webserver', 'watch', 'build', 'openBrowser'], function () {
+});
+
+gulp.task('webserver', function () {
+  connect.server({
+    root: ['./' + config.distFolder],
+    livereload: true,
+    port: config.port,
+    host: '0.0.0.0'
+  });
+});
+
+gulp.task('watch', function () {
+  gulp.watch(config.sourceFolder + '/**/*', ['build']);
+  watch('./' + config.distFolder).pipe(connect.reload());
+});
+
+gulp.task('openBrowser', function(){
+  gulp.src(__filename)
+    .pipe(open({uri: 'http://localhost:' + config.port}));
+});
+
+gulp.task('build', function () {
+  runSequence('clean', 'renderTemplates', 'copyImages', 'minifyFiles', function () {
+  });
+});
+
+gulp.task('clean', function () {
+  return gulp.src(config.distFolder, {read: false, force: true})
     .pipe(clean());
 });
 
-gulp.task('nunjucksRender', function () {
-  gulp.src([sourceFolder + '/*.+(html|nunjucks)'])
-    .pipe(nunjucksRender({path: [sourceFolder]}))
-    .pipe(gulp.dest(distFolder));
+gulp.task('renderTemplates', function () {
+  gulp.src([config.sourceFolder + '/*.+(html|nunjucks)'])
+    .pipe(nunjucksRender({path: [config.sourceFolder]}))
+    .pipe(gulp.dest(config.distFolder));
 });
 
-gulp.task('copySourceFolder', function () {
-  gulp.src([sourceFolder + '/assets/img/**/*'])
-    .pipe(gulp.dest(distFolder + '/assets/img'));
+gulp.task('copyImages', function () {
+  gulp.src([config.sourceFolder + '/assets/img/**/*'])
+    .pipe(gulp.dest(config.distFolder + '/assets/img'));
 });
 
 gulp.task('minifyFiles', function (cb) {
   setTimeout(function (cb) {
     pump([
-      gulp.src(distFolder + '/*.html'),
+      gulp.src(config.distFolder + '/*.html'),
       htmlmin(
         {
           collapseWhitespace: true,
@@ -37,10 +73,14 @@ gulp.task('minifyFiles', function (cb) {
           minifyJS: false
         }
       ),
-      gulp.dest(distFolder)
+      gulp.dest(config.distFolder)
     ], cb);
   }, 100)
 });
 
-gulp.task('build', ['default', 'nunjucksRender', 'copySourceFolder', 'minifyFiles'], function () {
+gulp.task('validateHTML', function() {
+  return gulp.src('dist/**/*.html')
+    .pipe(gulpAmpValidator.validate())
+    .pipe(gulpAmpValidator.format())
+    .pipe(gulpAmpValidator.failAfterError());
 });
